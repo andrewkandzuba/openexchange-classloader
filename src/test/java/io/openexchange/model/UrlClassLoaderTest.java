514365bddcb36178b271166c83f7e4016c9d92fb
Andriy Kandzyuba <Andriy.Kandzyuba@BNSF.com>
2017-09-19 15:13:00 -0500

    reflection with single classloader

--- src/test/java/io/openexchange/model/UrlClassLoaderTest.java
+++ src/test/java/io/openexchange/model/UrlClassLoaderTest.java
@@ -1,5 +1,6 @@
 package io.openexchange.model;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Rule;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,21 +8,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.rules.TemporaryFolder;
 
 import javax.tools.JavaCompiler;
+import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
 
-import static org.apache.commons.io.FileUtils.copyFileToDirectory;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.*;
 
 public class UrlClassLoaderTest {
 
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
-
-    private File generatedCode;
 
     @BeforeEach
     void setUp() throws IOException {
@@ -30,17 +32,35 @@ public class UrlClassLoaderTest {
         URL modelPackage = this.getClass().getClassLoader().getResource("io/openexchange/model");
         assertNotNull("Should not be null", modelPackage);
 
-        generatedCode = temporaryFolder.newFolder("generatedSources");
-        copyFileToDirectory(new File(modelPackage.getFile() + "/Person.java"), generatedCode);
-        copyFileToDirectory(new File(modelPackage.getFile() + "/House.java"), generatedCode);
+        File generatedCode = temporaryFolder.newFolder("io", "openexchange", "model");
+        FileUtils.copyDirectory(new File(modelPackage.getFile()), generatedCode);
 
-        assertTrue("Should exist", new File(generatedCode + "/Person.java" ).exists());
-        assertTrue("Should exist", new File(generatedCode + "/House.java" ).exists());
+        List<File> sources = asList(new File(generatedCode + "/Person.java"), new File(generatedCode + "/House.java"));
+        sources.forEach(file -> assertTrue("Should exist", file.exists()));
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        try (StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null)) {
+            compiler.getTask(null, fileManager, null, null, null, fileManager.getJavaFileObjectsFromFiles(sources)).call();
+        }
+
+        List<File> generated = asList(new File(generatedCode + "/Person.class"), new File(generatedCode + "/House.class"));
+        generated.forEach(file -> assertTrue("Should exist", file.exists()));
     }
 
     @Test
-    void generateAndLoad() throws IOException {
-        JavaCompiler javaCompiler = ToolProvider.getSystemJavaCompiler();
+    void loadSingleClassloader() throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        ClassLoader classLoader = new URLClassLoader(new URL[]{temporaryFolder.getRoot().toURI().toURL()});
+
+        Class<?> personClazz = classLoader.loadClass("io.openexchange.model.Person");
+        Object person = personClazz.getConstructor(String.class, String.class).newInstance("John", "Smith");
+
+        Class<?> houseClazz = classLoader.loadClass("io.openexchange.model.House");
+        Object house = houseClazz.getConstructor(String.class, personClazz).newInstance("Fort Worth", person);
+
+        assertEquals("Should be equals",
+                "Fort Worth", houseClazz.getMethod("getAddress").invoke(house));
+        assertEquals("Should be equals",
+                person, houseClazz.getMethod("getOwner").invoke(house));
     }
 
     @AfterEach
